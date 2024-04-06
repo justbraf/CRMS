bea72b69d84e3a0e05c718573fa94631a0cd7333
justbraf <dwaynecorey@gmail.com>
2024-04-06 14:44:09 -0400

    Upd tables and added default users and roles

--- carrentalmanagementsys.sql
+++ carrentalmanagementsys.sql
@@ -3,19 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 06, 2024 at 07:50 AM
+-- Generation Time: Apr 06, 2024 at 08:21 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `carrentalmanagementsys`
@@ -60,8 +54,17 @@ CREATE TABLE `employees` (
   `EmpID` int(11) NOT NULL,
   `Firstname` varchar(30) NOT NULL,
   `Lastname` varchar(30) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(61) NOT NULL,
   `Access_Level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`EmpID`, `Firstname`, `Lastname`, `username`, `password`, `Access_Level`) VALUES
+(1, 'System', 'Administrator', 'admin', '$2y$10$kbWN6weyDOz2kWlIVBpj4OZPVYlTWGyiMZlXEiUWZtyGPKvOeVew6', 1);
 
 -- --------------------------------------------------------
 
@@ -97,6 +100,16 @@ CREATE TABLE `roles` (
   `Access_Level` int(11) NOT NULL,
   `Description` varchar(160) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`Rolename`, `Access_Level`, `Description`) VALUES
+('Administrator', 1, 'Full access to all sections'),
+('Manager', 2, 'Manage employees\r\nBan/remove customers\r\nRemove vehicles from fleet, etc\r\n'),
+('Accounts Agent', 3, 'Register new vehicles to fleet\r\nUpdate vehicle data\r\nGenerate reports'),
+('Rental Agent', 4, 'Manage Customers\r\nManage Rentals\r\nGenerate Invoices\r\nReceive Payments');
 
 -- --------------------------------------------------------
 
@@ -166,7 +179,7 @@ ALTER TABLE `customers`
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `EmpID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `EmpID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `rentalsandreturns`
@@ -174,6 +187,11 @@ ALTER TABLE `employees`
 ALTER TABLE `rentalsandreturns`
   MODIFY `RID` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `Access_Level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Constraints for dumped tables
 --
@@ -191,7 +209,3 @@ ALTER TABLE `rentalsandreturns`
   ADD CONSTRAINT `FK_CID` FOREIGN KEY (`CID`) REFERENCES `customers` (`CID`),
   ADD CONSTRAINT `FK_VID` FOREIGN KEY (`VID`) REFERENCES `vehicles` (`VID`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
